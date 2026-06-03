8880eff8b716a5e52eeb4a81c2455cacff5d7349
Susovon Sarkar <susovonsarkar0@gmail.com>
2026-06-03 18:54:54 +0530

    style: set Peel image container fit to cover to match other cards

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -109,7 +109,7 @@ export default function Projects() {
                             alt="Peel Icon"
                             fill
                             priority
-                            className="object-contain p-8 md:p-12 transition-transform duration-700 group-hover:scale-105"
+                            className="object-cover transition-transform duration-700 group-hover:scale-105"
                         />
                     </a>
 
